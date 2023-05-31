7d991801e463c3820f38716cb1e0c92e2c887470
John Baldwin <johnbaldwin82@gmail.com>
2023-05-31 10:40:22 -0600

    working on models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,12 +6,30 @@ const ProductTag = require('./ProductTag');
 
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'productTag_tag',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'productTag_product',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
